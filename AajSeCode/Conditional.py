ae4ad6a1f5674215d6fdb9e1ee61a26237cdb5dc
Saurav kumar Arya <92774037+sauravkrarya@users.noreply.github.com>
2024-08-10 12:39:23 +0530

    update

---
+++ AajSeCode/Conditional.py
@@ -0,0 +1,11 @@
+# age=int(input("what's your age"))
+#  print(age)
+#  print(type(age))
+# if age>=18:
+#     print("you'r granted")
+# elif age == 17:
+#     print("please wait for 1 year")
+# else:
+#     print("you'r not elgible")
+
+
